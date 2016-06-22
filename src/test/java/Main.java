220d4a90e915e9dfb8266fc4f90464d30c6364c0
Damon Kelley <damon.kelley@gmail.com>
2016-06-22 17:45:39 -0500

    Add a main function

---
+++ src/test/java/Main.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        new UI(reader, writer).start();
+    }
+}
