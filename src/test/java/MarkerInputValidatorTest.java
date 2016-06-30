8c9517db3afec5013e9627a581c63762376a4cbf
Damon Kelley <damon.kelley@gmail.com>
2016-06-30 11:40:49 -0500

    Add input validators for markers and player types

---
+++ src/test/java/MarkerInputValidatorTest.java
@@ -0,0 +1,25 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class MarkerInputValidatorTest {
+    @Test
+    public void itValidatesX() {
+        assertTrue(new MarkerInputValidator().isValid("X"));
+    }
+
+    @Test
+    public void itValidatesO() {
+        assertTrue(new MarkerInputValidator().isValid("O"));
+    }
+
+    @Test
+    public void allOtherInputIsInvalid() {
+        assertFalse(new MarkerInputValidator().isValid("o"));
+        assertFalse(new MarkerInputValidator().isValid("foo"));
+        assertFalse(new MarkerInputValidator().isValid("1"));
+        assertFalse(new MarkerInputValidator().isValid("#$@#$"));
+        assertFalse(new MarkerInputValidator().isValid(null));
+    }
+}
---
+++ src/test/java/PlayerTypeInputValidatorTest.java
@@ -0,0 +1,27 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class PlayerTypeInputValidatorTest {
+    @Test
+    public void HIsValidInput() {
+        assertTrue(new PlayerTypeInputValidator().isValid("H"));
+    }
+
+    @Test
+    public void CIsValidInput() {
+        assertTrue(new PlayerTypeInputValidator().isValid("C"));
+    }
+
+    @Test
+    public void allOtherInputIsInvalid() {
+        assertFalse(new PlayerTypeInputValidator().isValid("X"));
+        assertFalse(new PlayerTypeInputValidator().isValid("c"));
+        assertFalse(new PlayerTypeInputValidator().isValid("h"));
+        assertFalse(new PlayerTypeInputValidator().isValid("1"));
+        assertFalse(new PlayerTypeInputValidator().isValid("computer"));
+        assertFalse(new PlayerTypeInputValidator().isValid("human"));
+        assertFalse(new PlayerTypeInputValidator().isValid(null));
+    }
+}
