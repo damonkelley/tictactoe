d81c3646e2c74af9ad2d15fbcaf02bd29a01d4dc
Damon Kelley <damon.kelley@gmail.com>
2016-07-25 15:05:23 -0500

    Add the GameOptions form

---
+++ app/src/main/java/me/damonkelley/tictactoe_app/fragment/GameOptions.java
@@ -0,0 +1,43 @@
+package me.damonkelley.tictactoe_app.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import me.damonkelley.tictactoe_app.R;
+
+public class GameOptions extends Fragment {
+
+    private Spinner playerOneSpinner;
+    private Spinner playerTwoSpinner;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_game_options, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        playerOneSpinner = (Spinner) getView().findViewById(R.id.player_one_type);
+        playerTwoSpinner = (Spinner) getView().findViewById(R.id.player_two_type);
+
+        setupSpinnerFor(playerOneSpinner);
+        setupSpinnerFor(playerTwoSpinner);
+    }
+
+    private void setupSpinnerFor(Spinner spinner) {
+        ArrayAdapter<CharSequence> otherAdapter = ArrayAdapter.createFromResource(
+                this.getActivity(),
+                R.array.player_type,
+                android.R.layout.simple_spinner_item);
+
+        otherAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(otherAdapter);
+    }
+}
